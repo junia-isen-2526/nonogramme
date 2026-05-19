5771163806df82a7022312e55c11b2039fbf081c
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2026-05-19 15:18:26 +0200

    Remove unused Nonogram loading and saving code from main function

--- main.c
+++ main.c
@@ -3,16 +3,5 @@
 #include "repository/nonogramfile.h"
 
 int main(void) {
-    Nonogram *nonogram = loadNonogramFromFile("../doc/jad.nono");
-
-    for (unsigned char row = 0; row < nonogramGetHeight(nonogram); row++) {
-        for (unsigned char column = 0; column < nonogramGetWidth(nonogram); column++) {
-            Pixel pixel = nonogramGetXY(nonogram, column, row);
-            printf("%c ", pixel == BLACK ? '#' : ' ');
-        }
-        printf("\n");
-    }
-    saveNonogramToFile("../doc/jad_saved.nono", nonogram);
-
     return 0;
 }
