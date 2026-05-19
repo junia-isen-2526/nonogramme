3aa790e85048e32dc6caef1494dd00ac5ee26637
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2026-05-19 10:54:00 +0200

    Implement loadNonogramFromFile function for loading Nonogram data from a file

--- repository/nonogramfile.c
+++ repository/nonogramfile.c
@@ -3,3 +3,9 @@
 //
 
 #include "nonogramfile.h"
+
+Nonogram *loadNonogramFromFile(const char *filename);
+
+int saveNonogramToFile(const char *filename, Nonogram *nonogram) {
+	return 0;
+}
--- repository/nonogramfile.h
+++ repository/nonogramfile.h
@@ -9,7 +9,20 @@
 #include <stdio.h>
 #include "../entity/nonogram.h"
 
-Nonogram *loadNonogramFromFile(const char *filename);
+Nonogram *loadNonogramFromFile(const char *filename) {
+	if (filename == NULL) return NULL;
+	FILE *file = fopen(filename, "r");
+	if (file == NULL) return NULL;
+	Nonogram *nonogram = NULL;
+	char buffer[256];
+	if (fgets(buffer, sizeof(buffer), file) != NULL) {
+		int rows, cols;
+		if (strtol(buffer, NULL, 10) > 0 && strtol(buffer, NULL, 10) > 0) {
+			nonogram = createNonogram(rows, cols);
+		}
+	}
+}
+
 int saveNonogramToFile(const char *filename, Nonogram *nonogram);
 
 #endif //NONOGRAMME_NONOGRAMMEFILE_H
